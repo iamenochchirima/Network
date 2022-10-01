cf947eb7d66473c69f89e6408e436ee2f866b508
Enoch <iamenochchirima@gmail.com>
2022-10-01 14:08:58 +0200

    Created the Post model

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,15 @@
+from email.policy import default
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author", default=None)
+
+    def __str__(self):
+        return f"{self.body} by {self.author} at {self.date}"
